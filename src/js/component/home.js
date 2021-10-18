f7487d2d785f44e1c736a908e350adf053770665
Edissondiaz <edissondiaz123@gmail.com>
2021-10-18 22:41:25 +0000

    arreglo

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,13 +1,11 @@
 import React from "react";
 
-
-
+import rigoImage from "../../img/rigo-baby.jpg";
 import { Navbar } from "./navbar";
 import { Jumbotron } from "./jumbotron";
 import { Footer } from "./footer";
 import { Card } from "./card";
 import { CardContainer } from "./cardContainer";
-
 
 export function Home() {
 	return (
